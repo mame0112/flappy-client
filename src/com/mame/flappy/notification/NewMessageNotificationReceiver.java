8a7f77bc55bc665f0eb8a7b007403d7078ac3eeb
mame0112 <mame0112@gmail.com>
2014-08-31 16:53:30 +0900

    Add wakelock to NewMessageNotificationManager to show notification in case of screen lock

--- src/com/mame/flappy/notification/NewMessageNotificationReceiver.java
+++ src/com/mame/flappy/notification/NewMessageNotificationReceiver.java
@@ -6,6 +6,7 @@ import com.mame.flappy.util.DbgUtil;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.PowerManager;
 
 public class NewMessageNotificationReceiver extends BroadcastReceiver {
 
@@ -17,10 +18,20 @@ public class NewMessageNotificationReceiver extends BroadcastReceiver {
 		DbgUtil.showDebug(TAG, "onReceive");
 
 		if (intent != null) {
+
+			// Screen on
+			PowerManager pm = (PowerManager) context
+					.getSystemService(Context.POWER_SERVICE);
+			PowerManager.WakeLock wl = pm.newWakeLock(
+					PowerManager.ACQUIRE_CAUSES_WAKEUP, "My Tag");
+			wl.acquire(1000);
+
 			int targetUserId = intent.getIntExtra(
 					LcomConst.EXTRA_TARGET_USER_ID, LcomConst.NO_USER);
 			int fromUserId = intent.getIntExtra(LcomConst.EXTRA_USER_ID,
 					LcomConst.NO_USER);
+
+			wl.release();
 
 			Intent i = new Intent(context, NewMessageNotificationService.class);
 			i.putExtra(LcomConst.EXTRA_USER_ID, fromUserId);
